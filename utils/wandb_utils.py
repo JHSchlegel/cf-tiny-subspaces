9dc081fcfc304bdb83b43d0987b73b3e53af8d8a
Jan Schlegel <jschlegel@student.ethz.ch>
2024-12-21 15:05:54 +0100

    uncommented part that throws errors

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -7,30 +7,26 @@ from typing import Optional, Dict, Any
 def setup_wandb(
     dir: Optional[str],
     project: str = "ETHZ AS2024 Deep Learning/cf-tiny-subspaces",
-    config: Optional[Dict[str, Any]] = None
+    config: Optional[Dict[str, Any]] = None,
 ) -> None:
     """
     Setup Weights & Biases logging.
-    
+
     Args:
         dir (Optional[str]): W&B logging directory
         project (str): Name of the wandb project
         config (Optional[Dict[str, Any]]): Configuration dictionary (optional)
     """
-    load_dotenv()
-    
-    api_key = os.getenv('WANDB_API_KEY')
+    # load_dotenv()
 
-    if api_key is None:
-        raise ValueError("WANDB_API_KEY not found! Please make sure you have access key to W&B.")
-    
-    wandb.login(api_key)
-    
+    # api_key = os.getenv('WANDB_API_KEY')
+
+    # if api_key is None:
+    #     raise ValueError("WANDB_API_KEY not found! Please make sure you have access key to W&B.")
+
+    # wandb.login(api_key)
+
     if wandb.run is None:
-        wandb.init(
-            dir=dir,
-            project=project,
-            config=config
-        )
+        wandb.init(dir=dir, project=project, config=config)
 
     return True, "Logged into Weights & Biases!"
