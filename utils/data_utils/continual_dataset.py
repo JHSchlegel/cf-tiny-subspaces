fab3d37e4ab09d00d5414f972b7516df254d57f5
Jan Schlegel <jschlegel@student.ethz.ch>
2024-12-21 10:10:23 +0100

    more sensible methods for abstract class

--- utils/data_utils/continual_dataset.py
+++ utils/data_utils/continual_dataset.py
@@ -15,44 +15,63 @@ class ContinualDataset(ABC):
     Abstract base class for continual learning datasets.
     """
 
-    def __init__(self, root: str = "../../data", download: bool = True, seed: int = 42):
-        """_summary_
+    def __init__(
+        self,
+        num_tasks: int,
+        seed: int = 42,
+    ):
+        """
+        Initialize the continual learning dataset.
 
         Args:
-            root (str, optional): _description_. Defaults to "../../data".
-            download (bool, optional): _description_. Defaults to True.
-            seed (int, optional): _description_. Defaults to 42.
+            num_tasks (int): Number of tasks.
+            seed (int, optional): Seed used for random componetns. Defaults to 42.
         """
-        self.root = root
-        self.download = download
+        self.num_tasks = num_tasks
         self.seed = seed
+
+        self.current_task_id = 0
 
         # set seeds for reproducibility:
         self.rng = np.random.default_rng(seed)
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
+
+        self.train_loader = {}
+        self.test_loaders = {}
 
     @abstractmethod
-    def get_task(self, task_id: int) -> Tuple[Dataset, Dataset]:
+    def setup_tasks(
+        self,
+        batch_size: int,
+        data_root: str = "../../data",
+        num_workers: int = 4,
+        **kwargs
+    ) -> None:
         """
-        Get the training and test datasets for a given task.
+        Create training and test dataloaders for all tasks.
+
+        Args:
+            batch_size (int): Batch size.
+            root (str, optional): Root directory of the data. Defaults to "../../data".
+            num_workers (int, optional): Number of workers for data loading. Defaults to 4.
+        """
+        pass
+
+    def get_task_dataloaders(
+        self,
+        task_id: int,
+    ) -> Tuple[DataLoader, DataLoader]:
+        """
+        Get the training dataloader of the current task as well as the test dataloaders
+        of all tasks seen so far.
 
         Args:
             task_id (int): Task ID.
 
         Returns:
-            Tuple[Dataset, Dataset]: Training and test datasets.
+            Tuple[DataLoader, Dict[int, DataLoader]]: Training dataloader for a specific task
+                and a dictionary of test dataloaders for all tasks seen so far.
         """
-        pass
-
-    @property
-    @abstractmethod
-    def num_tasks(self) -> int:
-        """
-        Get the number of tasks in the dataset.
-
-        Returns:
-            int: Number of tasks.
-        """
-        pass
+        return (
+            self.train_loader[task_id],
+            {id: self.test_loaders[id] for id in range(task_id + 1)},
+        )
