e835f875b783de303f090ae0d2664a30fb2af655
Armin Begic <begic.armin@outlook.com>
2024-12-25 13:36:31 +0100

    added cifar10 continual dataset class

--- utils/data_utils/sequential_CIFAR.py
+++ utils/data_utils/sequential_CIFAR.py
@@ -5,7 +5,7 @@ from .continual_dataset import ContinualDataset
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader, Subset, ConcatDataset
-from torchvision import datasets, transforms, CIFAR100
+from torchvision import datasets, transforms, CIFAR100, CIFAR10
 from PIL import Image
 from typing import List, Tuple, Dict, Optional, Iterator
 
@@ -71,4 +71,66 @@ class CL_CIFAR100(ContinualDataset):
                 num_workers=num_workers,
                 pin_memory=True
             )
+
+class CL_CIFAR10(ContinualDataset):
+    """
+    Continual dataset class for the CIFAR-100 dataset
+    """
+
+    def __init__(self, num_tasks, seed:int=42)->None:
+        super().__init__(num_tasks, seed)
+
+        self.classes_per_task = 10 // num_tasks 
+        self.class_order = np.arange(10)
+        self.rng.shuffle(self.class_order)
+    
+    def setup_tasks(self, batch_size:int, data_root:str="./data", num_workers:int=4) -> None:
+        """
+        Create training and test dataloaders for all tasks
+
+        Args:
+            batch_size (int): Batch size.
+            data_root (str, optional): Root directory of the data. Defaults to "./data".
+            num_workers (int, optional): Number of workers for data loading. Defaults to 4.
+        """
+
+        MEAN, STD = (0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)
+        transform_train = transforms.Compose(
+            [transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(MEAN, STD)])
+        transform_test = transforms.Compose(
+                transforms.ToTensor(), 
+                transforms.Normalize(MEAN, STD)
+            )
+        train_dataset = CIFAR10(data_root, train=True, download=True, transform=transform_train)
+        test_dataset = CIFAR10(data_root, train=False, download=True, transform=transform_test)
+
+        for task_id in range(self.num_tasks):
+            start_class = task_id * self.classes_per_task
+            end_class = (task_id + 1) * self.classes_per_task
+
+            task_classes = self.class_order[start_class:end_class]
+
+            train_indices = [i for i, (_, label) in enumerate(train_dataset) if label in task_classes]
+            test_indices = [i for i, (_, label) in enumerate(test_dataset) if label in task_classes]
+
+            train_subset = Subset(train_dataset, train_indices)
+            test_subset = Subset(test_dataset, test_indices)
+
+            self.train_loaders[task_id] = DataLoader(
+                train_subset,
+                batch_size=batch_size,
+                shuffle=True,
+                num_workers=num_workers,
+                pin_memory=True
+            )
+            self.test_loaders[task_id] = DataLoader(
+                test_subset,
+                batch_size=batch_size,
+                shuffle=False,
+                num_workers=num_workers,
+                pin_memory=True
+            )            
             
